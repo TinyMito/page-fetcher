8e919c9ade94469774f66c06f3c6020fb43b3926
TinyMito <k.ip@me.com>
2023-04-21 22:39:41 -0400

    Update the functionality to fetch file size and get argv from command line

--- fetcher.js
+++ fetcher.js
@@ -1,20 +1,32 @@
 const request = require('request');
 const fs = require('fs');
+const command = process.argv.slice(2); // Input from node
 
+request(`${command[0]}`, (error, response, body) => {
+  const size = body.length; // Get the file size 1 byte per character
 
-request('http://example.edu', (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
+  if (error) {
+    // Print the error if one occurred
+    console.log('error:', error);
+  }
+  if (response) {
+    // Print the response status code if a response was received
+    console.log('statusCode:', response && response.statusCode);
+  }
 
-  fs.writeFile('./index.html', body, err => {
+  fs.writeFile(`${command[1]}`, body, err => {
+    // Write to file
+
     if (err) {
+      // If error, output error message.
       console.error(err);
     }
-    console.log('HTML Fetched Successfully!')
+
+    console.log(`Downloaded and saved ${size} bytes to ${command[1]}`); // If success
   });
 
 });
+
 
 
 
